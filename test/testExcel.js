8267937ee62f5da2f52735982c610cae6654fc8c
sail <sail@helpugreat.com>
2013-09-29 13:04:56 +0800

    d

--- test/testExcel.js
+++ test/testExcel.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 var exlBuf = fs.readFileSync("./test.xlsx");
 
 //数据源
-var data = [[{"dpt_des":"开发部","doc_dt":"2013-09-09","doc":"a001"}],[{"pt":"pt1","des":"des1","due_dt":"due_dt1","des2":"des21"},{"pt":"pt1","des":"des1","due_dt":"due_dt1","des2":"des21"}]];
+var data = [[{"dpt_des":"开发部","doc_dt":"2013-09-09","doc":"a001"}],[{"pt":"pt1","des":"des1","due_dt":"2013-08-07","des2":"2013-12-07"},{"pt":"pt1","des":"des1","due_dt":"2013-09-14","des2":"des21"}]];
 
 //用数据源(对象)data渲染Excel模板
 ejsExcel.renderExcelCb(exlBuf, data, function(exlBuf2){

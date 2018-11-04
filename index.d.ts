c06c82c80d0c1cf86ddac9f1d820faff77e1311e
kirno <kirning@hotmail.com>
2018-11-04 22:09:12 +0800

    添加d.ts文件

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+/**
+ * 渲染excel模板
+ * @param exlBuf 模板excel的Buff
+ * @param _data_ 数据
+ */
+export declare function renderExcel(exlBuf: Buffer, _data_: any): Buffer;
+
+/**
+ * 渲染excel模板
+ * @param exlBuf 模板excel的Buff
+ * @param _data_ 数据
+ * @param cb 渲染成功后回调
+ */
+export declare function renderExcelCb(exlBuf: Buffer, _data_: any, cb: Function): Buffer;

c37b43a0b3b776b61ca0105dbd3caf6aa272f411
sail-sail <151263555@qq.com>
2019-07-12 00:40:23 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@
  * @param exlBuf 模板excel的Buff
  * @param _data_ 数据
  */
-export declare function renderExcel(exlBuf: Buffer, _data_: any): Buffer;
+export declare function renderExcel(exlBuf: Buffer, _data_: any): Promise<Buffer>;
 
 /**
  * 渲染excel模板
@@ -11,4 +11,4 @@ export declare function renderExcel(exlBuf: Buffer, _data_: any): Buffer;
  * @param _data_ 数据
  * @param cb 渲染成功后回调
  */
-export declare function renderExcelCb(exlBuf: Buffer, _data_: any, cb: Function): Buffer;
+export declare function renderExcelCb(exlBuf: Buffer, _data_: any, cb: Function): void;
